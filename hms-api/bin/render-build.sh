17e50f7c06b8dccd4b88a8c866814283fd745b19
sagheer1122 <sagheerahmad5767@gmail.com>
2026-02-21 11:54:51 +0500

    Fix: Remove ruby.exe shebangs and fix line endings

--- hms-api/bin/render-build.sh
+++ hms-api/bin/render-build.sh
@@ -2,8 +2,9 @@
 # exit on error
 set -o errexit
 
-# Fix line endings
+# Fix line endings and shebangs for all bin files
 find bin -type f -exec sed -i 's/\r$//' {} +
+find bin -type f -exec sed -i 's/ruby\.exe/ruby/g' {} +
 chmod +x bin/*
 
 bundle install
